a27aa6dcf3d91f902e6ea7020ffbc9388360a6f2
Aminul Islam <aminul7506@gmail.com>
2025-04-13 03:30:42 -0500

    add run of movielens data

--- code/run.sh
+++ code/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#./run.sh KuaiRec lgn navip 4 1000
+#./run.sh Movielens lgn 3 1000
 
 dataset=$1
 model=$2
@@ -8,4 +8,4 @@ layer=$3
 epoch=$4
 
 python main.py --dataset $dataset --model $model --layer $layer --epochs $epoch
-echo "Running done for $variant$ variant"
+echo "Running done for $model$ model."
